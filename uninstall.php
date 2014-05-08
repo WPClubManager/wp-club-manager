b5d4e8af38d39b3a1c177ffa25f2b00cd7ec95f0
roofdog <leon@xenproject.com>
2014-05-08 12:08:34 +0100

    Improve uninstall functions

--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,7 @@
  * @author 		ClubPress
  * @category 	Core
  * @package 	WPClubManager/Uninstaller
- * @version     1.0.0
+ * @version     1.1
  */
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) 
 	exit();
@@ -20,7 +20,11 @@ $installer->remove_roles();
 
 // Delete options
 $wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE 'wpcm_%';");
+$wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE 'wpclubmanager_%';");
+$wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE '_wpcm_%';");
 
 $wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'wpcm_player', 'wpcm_staff', wpcm_club', 'wpcm_match', 'wpcm_sponsor' );" );
 
 $wpdb->query( "DELETE FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE wp.ID IS NULL;" );
+
+delete_option( 'wpclubmanager_installed' );
