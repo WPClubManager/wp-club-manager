eb6771f3f5ba1c89c09f583cb1f549e9b3529b1f
roofdog <leon@xenproject.com>
2014-05-08 12:09:45 +0100

    Added deprecated functions file

---
+++ includes/wpcm-deprecated-functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Deprecated functions
+ *
+ * Where functions come to retire.
+ *
+ * @author 		Clubpress
+ * @category 	Core
+ * @package 	WPClubManager/Functions
+ * @version     1.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+function wpcm_enqueue_js( $code ) {
+	_deprecated_function( 'wpcm_enqueue_js', '1.0.3', '' );
+}
+function wpcm_print_js() {
+	_deprecated_function( 'wpcm_print_js', '1.0.3', '' );
+}
+function check_jquery() {
+	_deprecated_function( 'check_jquery', '1.1', '' );
+}
