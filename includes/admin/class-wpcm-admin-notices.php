d14b728a0e4edb1a3d0c0174453957f293dc66b6
roofdog <leon@xenproject.com>
2014-05-30 12:19:31 +0100

    Add theme notice

--- includes/admin/class-wpcm-admin-notices.php
+++ includes/admin/class-wpcm-admin-notices.php
@@ -5,7 +5,7 @@
  * @author 		Clubpress
  * @category 	Admin
  * @package 	WPClubManager/Admin
- * @version     1.1.0
+ * @version     1.1.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -37,11 +37,6 @@ class WPCM_Admin_Notices {
 	 * Add notices + styles if needed.
 	 */
 	public function add_notices() {
-		// if ( get_option( '_wpcm_needs_welcome' ) == 1 ) {
-		// 	wp_enqueue_style( 'wpclubmanager-activation', plugins_url(  '/assets/css/activation.css', WPCM_PLUGIN_FILE ) );
-		// 	add_action( 'admin_notices', array( $this, 'install_notice' ) );
-		// }
-
 		$notices = get_option( 'wpclubmanager_admin_notices', array() );
 
 		if ( ! empty( $_GET['hide_theme_support_notice'] ) ) {
@@ -63,70 +58,58 @@ class WPCM_Admin_Notices {
 			}
 		}
 
-		if ( in_array( 'template_files', $notices ) ) {
-			wp_enqueue_style( 'wpclubmanager-activation', plugins_url(  '/assets/css/activation.css', WPCM_PLUGIN_FILE ) );
-//			add_action( 'admin_notices', array( $this, 'template_file_check_notice' ) );
-		}
+		// if ( in_array( 'template_files', $notices ) ) {
+		// 	wp_enqueue_style( 'wpclubmanager-activation', plugins_url(  '/assets/css/activation.css', WPCM_PLUGIN_FILE ) );
+		// 	add_action( 'admin_notices', array( $this, 'template_file_check_notice' ) );
+		// }
 	}
-
-	/**
-	 * Show the install notices
-	 */
-	// public function install_notice() {
-	// 	$screen = get_current_screen();
-
-	// 	// If we have just installed, show a message with the install pages button
-	// 	if ( get_option( '_wpcm_needs_welcome' ) == 1 && $screen->id != 'toplevel_page_wpclubmanager' ) {
-	// 		include( 'views/html-notice-install.php' );
-	// 	}
-	// }
 
 	/**
 	 * Show the Theme Check notice
 	 */
 	public function theme_check_notice() {
-//		include( 'views/html-notice-theme-support.php' );
+		include( 'views/html-notice-theme-support.php' );
 	}
 
 	/**
 	 * Show a notice highlighting bad template files
 	 */
-	public function template_file_check_notice() {
-		if ( isset( $_GET['page'] ) && 'wpcm-status' == $_GET['page'] ) {
-			return;
-		}
+	// public function template_file_check_notice() {
+	// 	if ( isset( $_GET['page'] ) && 'wpcm-status' == $_GET['page'] ) {
+	// 		return;
+	// 	}
 
-		$status         = include( 'class-wpcm-admin-status.php' );
-		$core_templates = $status->scan_template_files( WPCM()->plugin_path() . '/templates' );
-		$outdated       = false;
+	// 	$status         = include( 'class-wpcm-admin-status.php' );
+	// 	$core_templates = $status->scan_template_files( WPCM()->plugin_path() . '/templates' );
+	// 	$outdated       = false;
 
-		foreach ( $core_templates as $file ) {
-			$theme_file = false;
-			if ( file_exists( get_stylesheet_directory() . '/' . $file ) ) {
-				$theme_file = get_stylesheet_directory() . '/' . $file;
-			} elseif ( file_exists( get_stylesheet_directory() . '/wpclubmanager/' . $file ) ) {
-				$theme_file = get_stylesheet_directory() . '/wpclubmanager/' . $file;
-			} elseif ( file_exists( get_template_directory() . '/' . $file ) ) {
-				$theme_file = get_template_directory() . '/' . $file;
-			} elseif( file_exists( get_template_directory() . '/wpclubmanager/' . $file ) ) {
-				$theme_file = get_template_directory() . '/wpclubmanager/' . $file;
-			}
+	// 	foreach ( $core_templates as $file ) {
+	// 		$theme_file = false;
+	// 		if ( file_exists( get_stylesheet_directory() . '/' . $file ) ) {
+	// 			$theme_file = get_stylesheet_directory() . '/' . $file;
+	// 		} elseif ( file_exists( get_stylesheet_directory() . '/wpclubmanager/' . $file ) ) {
+	// 			$theme_file = get_stylesheet_directory() . '/wpclubmanager/' . $file;
+	// 		} elseif ( file_exists( get_template_directory() . '/' . $file ) ) {
+	// 			$theme_file = get_template_directory() . '/' . $file;
+	// 		} elseif( file_exists( get_template_directory() . '/wpclubmanager/' . $file ) ) {
+	// 			$theme_file = get_template_directory() . '/wpclubmanager/' . $file;
+	// 		}
 
-			if ( $theme_file ) {
-				$core_version  = $status->get_file_version( WPCM()->plugin_path() . '/templates/' . $file );
-				$theme_version = $status->get_file_version( $theme_file );
+	// 		if ( $theme_file ) {
+	// 			$core_version  = $status->get_file_version( WPCM()->plugin_path() . '/templates/' . $file );
+	// 			$theme_version = $status->get_file_version( $theme_file );
 
-				if ( $core_version && $theme_version && version_compare( $theme_version, $core_version, '<' ) ) {
-					$outdated = true;
-					break;
-				}
-			}
-		}
+	// 			if ( $core_version && $theme_version && version_compare( $theme_version, $core_version, '<' ) ) {
+	// 				$outdated = true;
+	// 				break;
+	// 			}
+	// 		}
+	// 	}
 
-		if ( $outdated ) {
-			include( 'views/html-notice-template-check.php' );
-		}
-	}
+	// 	if ( $outdated ) {
+	// 		include( 'views/html-notice-template-check.php' );
+	// 	}
+	// }
 }
 
 endif;
