f280383ccf2b1f00a38cc11976d6865fadbaef81
roofdog <leon@xenproject.com>
2014-06-02 17:57:33 +0100

    Update staff shortcode functions

--- templates/shortcodes/staff.php
+++ templates/shortcodes/staff.php
@@ -1,21 +1,24 @@
 <?php
 /**
- * Staff
+ * Staff shortcode template
+ *
+ * Override this template by copying it to yourtheme/wpclubmanager/content-single-staff.php
  *
  * @author 		Clubpress
  * @package 	WPClubManager/Templates
- * @version     1.1.1
+ * @version     1.1.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 $default = array(
-	'id' => get_the_ID(),
 	'id' => null,
 	'limit' => -1,
 	'season' => null,
 	'team' => null,
 	'jobs' => null,
+	'orderby' => 'name',
+	'order' => 'ASC',
 	'title' => __( 'Staff', 'wpclubmanager' ),
 	'linktext' => __( 'View all staff', 'wpclubmanager' ),
 	'linkpage' => null
@@ -23,165 +26,214 @@ $default = array(
 
 extract( $default, EXTR_SKIP );
 
+$stats_labels = array(
+	'flag' => '&nbsp;',
+	'number' => '&nbsp;',
+	'name' => __( 'Name', 'wpclubmanager' ),
+	'thumb' => '&nbsp',
+	'job' => __( 'Job', 'wpclubmanager' ),
+	'age' => __( 'Age', 'wpclubmanager' ),
+	'team' => __( 'Team', 'wpclubmanager' ),
+	'season' => __( 'Season', 'wpclubmanager' ),
+	'joined' => __( 'Joined', 'wpclubmanager' )
+);
+
+
 if ( $limit == 0 )
 	$limit = -1;
-if ( $id <= 0 )
-	$id = null;
 if ( $team <= 0 )
 	$team = null;
+// if ( $jobs <= 0 )
+// 	$jobs = null;
 
-global $post;
+$stats = explode( ',', $stats );
 
-$show_dob = get_option( 'wpcm_staff_profile_show_dob' );
-$show_age = get_option( 'wpcm_staff_profile_show_age' );
-$show_season = get_option( 'wpcm_staff_profile_show_season' );
-$show_team = get_option( 'wpcm_staff_profile_show_team' );
-$show_natl = get_option( 'wpcm_staff_profile_show_nationality' );
-$show_jobs = get_option( 'wpcm_staff_profile_show_jobs' );
-
-$output = '';
-if ( $id ) {
-	$post = get_post( $id );
-	$posts = array();
-	$posts[] = $post;
-} else {
-	$args = array(
-		'post_type' => 'wpcm_staff',
-		'tax_query' => array(),
-		'numposts' => $limit,
-		'posts_per_page' => $limit
-	);
-	if ( $season ) {
-		$args['tax_query'][] = array(
-			'taxonomy' => 'wpcm_season',
-			'terms' => $season,
-			'field' => 'term_id'
-		);
-	}
-	if ( $team ) {
-		$args['tax_query'][] = array(
-			'taxonomy' => 'wpcm_team',
-			'terms' => $team,
-			'field' => 'term_id'
-		);
-	}
-	if ( $jobs ) {
-		$args['tax_query'][] = array(
-			'taxonomy' => 'wpcm_jobs',
-			'terms' => $jobs,
-			'field' => 'term_id'
-		);
-	}
-	$posts = query_posts($args);
+foreach( $stats as $key => $value ) {
+	$stats[$key] = strtolower( trim( $value ) );
+	if ( !array_key_exists( $stats[$key], $stats_labels ) )
+		unset( $stats[$key] );
 }
-$count = 0;
-$size = sizeof($posts);
 
-if ($size > 0):
+$orderby = strtolower( $orderby );	
+$order = strtoupper( $order );
+$output = '';
 
-	while ( have_posts() ) : the_post();
+$query_args = array(
+	'post_type' => 'wpcm_staff',
+	'tax_query' => array(),
+	'numposts' => $limit,
+	'posts_per_page' => $limit,
+	'orderby' => $orderby,
+	'order' => $order
+);
 
-		$output .= '<div class="wpcm-staff-shortcode row">';
+if ( $season ) {
+	$query_args['tax_query'][] = array(
+		'taxonomy' => 'wpcm_season',
+		'terms' => $season,
+		'field' => 'term_id'
+	);
+}
 
-		if (get_the_post_thumbnail( $post->ID, 'staff_single' ) != null) {
+if ( $team ) {
+	$query_args['tax_query'][] = array(
+		'taxonomy' => 'wpcm_team',
+		'terms' => $team,
+		'field' => 'term_id'
+	);
+}
 
-			$output .= '<div class="wpcm-staff-image">'.get_the_post_thumbnail( $post->ID, 'staff_single', array('title' => get_the_title()) ).'</div>';
+if ( $jobs ) {
+	$query_args['tax_query'][] = array(
+		'taxonomy' => 'wpcm_jobs',
+		'terms' => $jobs,
+		'field' => 'term_id'
+	);
+}
+
+$staff = get_posts( $query_args );
+
+$count = 0;	
+
+if ( sizeof( $staff ) > 0 ) {
+
+	if( $title ) {
+		$title = '<h3 class="wpcm-sc-title">' . $title . '</h3>';
+	} else{
+		$title = '';
+	}
+
+	$output .= '<div class="wpcm-players-shortcode">
+		' . $title . '
+		<table>
+			<thead>
+				<tr>';
+				foreach( $stats as $stat ) {
+					$output .= '<th class="'. $stat . '">' . $stats_labels[$stat] .'</th>';
+				}
+				$output .= '</tr>
+			</thead>
+			<tbody>';
+
+	$player_details = array();
+
+	foreach( $staff as $player ) {
+
+		$player_details[$player->ID] = array();
+		$count++;
+
+		
+			$name = $player->post_title;
+			$jobs = get_the_terms( $player->ID, 'wpcm_jobs' );
+
+		if ( has_post_thumbnail( $player->ID ) ) {
+			$thumb = get_the_post_thumbnail( $player->ID, 'staff_thumbnail' );
+		} else {
+			$thumb = wpcm_placeholder_img( 'staff_thumbnail' );
+		}
+
+		if ( is_array( $jobs ) ) {
+			$job = reset($jobs);
+			$job = $job->name;
+
 		} else {
 
-			$output .= '<div class="wpcm-staff-image">'.apply_filters( 'wpclubmanager_single_product_image', sprintf( '<img src="%s" alt="Placeholder" />', wpcm_placeholder_img_src() ), $post->ID ).'</div>';
+			$job = __( 'None', 'wpclubmanager' );
+
 		}
 
-		$profile_details = array();
+		$natl = get_post_meta( $player->ID, 'wpcm_natl', true );
 
-		// job title
-		if ( $show_jobs == 'yes' ) {
+		foreach( $stats as $stat ) {
 
-			$jobs = get_the_terms( $post->ID, 'wpcm_jobs' );
-
-			if ( is_array( $jobs ) ) {
-
-				$staff_jobs = array();
-
-				foreach ( $jobs as $value ) {
-
-					$staff_jobs[] = $value->name;
-				}
-
-				$profile_details[ __('Job Title', 'wpclubmanager') ] = implode( ', ', $staff_jobs );
+			switch ( $stat ) {
+				case 'thumb':
+					$player_details[$player->ID][$stat] = '';
+					$player_details[$player->ID][$stat] .= '<a href="' . get_permalink( $player->ID ) . '">' . $thumb . '</a>';
+					break;
+				case 'flag':
+					$player_details[$player->ID][$stat] = '';
+					$player_details[$player->ID][$stat] .= '<img class="flag" src="' . WPCM_URL . 'assets/images/flags/' . $natl . '.png" />';
+					break;
+				case 'name':
+					$player_details[$player->ID][$stat] = '';
+					$player_details[$player->ID][$stat] .= '<a href="' . get_permalink( $player->ID ) . '">' . $name . '</a>';
+					break;
+				case 'job':
+					$player_details[$player->ID][$stat] = '';
+					$jobs = get_the_terms( $player->ID, 'wpcm_jobs' );
+					if ( is_array( $jobs ) ) {
+						$player_jobs = array();
+						foreach ( $jobs as $job ) {
+							$player_jobs[] = $job->name;
+						}
+						$player_details[$player->ID][$stat] .= implode( ', ', $player_jobs );
+					}
+					break;
+				case 'team':
+					$player_details[$player->ID][$stat] = '';
+					$teams = get_the_terms( $player->ID, 'wpcm_team' );
+					if ( is_array( $teams ) ) {
+						$player_teams = array();
+						foreach ( $teams as $team ) {
+							$player_teams[] = $team->name;
+						}
+						$player_details[$player->ID][$stat] .= implode( ', ', $player_teams );
+					}
+					break;
+				case 'season':
+					$player_details[$player->ID][$stat] = '';
+					$seasons = get_the_terms( $player->ID, 'wpcm_season' );
+					if ( is_array( $seasons ) ) {
+						$player_seasons = array();
+						foreach ( $seasons as $season ) {
+							$player_seasons[] = $season->name;
+						}
+						$player_details[$player->ID][$stat] .= implode( ', ', $player_seasons );
+					}
+					break;
+				case 'age':
+					$player_details[$player->ID][$stat] = '';
+					$player_details[$player->ID][$stat] .= get_age( get_post_meta( $player->ID, 'wpcm_dob', true ) );
+					break;
+				case 'joined':
+					$player_details[$player->ID][$stat] = '';
+					$player_details[$player->ID][$stat] = date_i18n( get_option( 'date_format' ), strtotime( $player->post_date ) );
+					break;
 			}
 		}
+	}
+	// if ( array_key_exists( $orderby, $player_stats_labels ) ) {
+	// 	$player_details = subval_sort( $player_details, $orderby );
+	// 	if ( $order == 'DESC' )
+	// 		$player_details = array_reverse( $player_details );
+	// }
+	$count = 0;
+	foreach( $player_details as $player_detail ) {
+		$count++;
+		if ( $limit > 0 && $count > $limit )
+			break;
 
-		// birthday
-		if ( $show_dob == 'yes' )
-			$profile_details[ __( 'Birthday', 'wpclubmanager' ) ] = date_i18n( get_option( 'date_format' ), strtotime( get_post_meta( $post->ID, 'wpcm_dob', true ) ) );
-		
-		// age
-		if ( $show_age == 'yes' )
-			$profile_details[__('Age', 'wpclubmanager')] = get_age( get_post_meta( $post->ID, 'wpcm_dob', true ) );
-		
-		// season
-		if ( $show_season == 'yes' ) {
-			$seasons = get_the_terms( $post->ID, 'wpcm_season' );
-			if ( is_array( $seasons ) ) {
-				$player_seasons = array();
-				foreach ( $seasons as $value ) {
-					$player_seasons[] = $value->name;
-				}
-				$profile_details[ __('Season', 'wpclubmanager') ] = implode( ', ', $player_seasons );
-			}
+		$output .=
+		'<tr>';
+		foreach( $stats as $stat ) {
+			$output .= '<td class="'. $stat . '">';
+			$output .= $player_detail[$stat];
+			$output .= '</td>';
 		}
-		
-		// team
-		if ( $show_team == 'yes' ) {
-			$teams = get_the_terms( $post->ID, 'wpcm_team' );
-			if ( is_array( $teams ) ) {
-				$player_teams = array();
-				foreach ( $teams as $team ) {
-					$player_teams[] = $team->name;
-				}
-				$profile_details[ __('Team', 'wpclubmanager') ] = implode( ', ', $player_teams );
-			}
-		}
+		$output .= '</tr>';
+	}
+	$output .= '</tbody></table>';
+	
+	$output .= '</div>';
 
-		// nationality
-		if ( $show_natl == 'yes' ) {
-			$natl = get_post_meta( $post->ID, 'wpcm_natl', true );
-			$profile_details[ __( 'Nationality', 'wpclubmanager' ) ] = '<img class="flag" src="'. WPCM_URL .'assets/images/flags/' . $natl . '.png" />';
-		}
-
-		$output .= '<div class="wpcm-staff-info">
-			<h1 class="entry-title">'.get_the_title($post->ID).'</h1>
-			<div class="wpcm-staff-meta">';
-			$count = 0;
-			$size = sizeof( $profile_details );
-			if ( $size > 0 ) {
-				$output .= 
-				'<table>' .
-					'<tbody>';
-				foreach ( $profile_details as $key => $value ) {
-					$count++;
-					$output .=
-					'<tr>' .
-						'<th>'.$key.'</th>' .
-						'<td>'.$value.'</td>' .
-					'</tr>';
-				}
-				$output .=
-					'</tbody>' .
-				'</table>';
-			}
-			$output .= '</div></div><div class="wpcm-staff-bio">' . apply_filters('the_content', get_the_content($post->ID)).'</div>';
-
-			if ( isset( $linkpage ) && $linkpage ) $output .= '<a href="' . get_page_link( $linkpage ) . '" class="wpcm-view-link">' . $linktext . '</a>';
-
-		$output .= '</div>';
-
-	endwhile;
+	if ( isset( $linkpage ) && $linkpage ) $output .= '<a href="' . get_page_link( $linkpage ) . '" class="wpcm-view-link">' . $linktext . '</a>';
 
 	wp_reset_postdata();
 
-endif;
+} else {
 
-wp_reset_query();
+	$output = '';
+}
 
 echo $output;
