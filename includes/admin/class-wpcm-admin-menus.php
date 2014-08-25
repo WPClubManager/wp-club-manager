17ee3069143ec3524512ac388e00cc58b5fd609a
roofdog <leon@xenproject.com>
2014-08-25 18:07:15 +0100

    New menu placements

--- includes/admin/class-wpcm-admin-menus.php
+++ includes/admin/class-wpcm-admin-menus.php
@@ -20,6 +20,8 @@ class WPCM_Admin_Menus {
 	public function __construct() {
 
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_menu', array( $this, 'settings_menu' ), 50 );
+		add_action( 'admin_menu', array( $this, 'extensions_menu' ), 70 );
 	}
 
 	/**
@@ -29,7 +31,10 @@ class WPCM_Admin_Menus {
 
 		global $menu, $wpclubmanager;
 
-	    $main_page = add_menu_page( __( 'Club Manager', 'wpclubmanager' ), __( 'Club Manager', 'wpclubmanager' ), 'manage_wpclubmanager', 'wpcm-settings' , array( $this, 'settings_page' ), WPCM()->plugin_url() . '/assets/images/logo.png', '99.6' );
+		if ( current_user_can( 'manage_wpclubmanager' ) )
+	    	$menu[] = array( '', 'read', 'separator-wpclubmanager', '', 'wp-menu-separator wpclubmanager' );
+
+	    $main_page = add_menu_page( __( 'Club Manager', 'wpclubmanager' ), __( 'Club Manager', 'wpclubmanager' ), 'manage_wpclubmanager', 'wpcm-settings' , array( $this, 'settings_page' ), WPCM()->plugin_url() . '/assets/images/logo.png', '55.4' );
 
 		remove_submenu_page('edit.php?post_type=wpcm_player', 'edit-tags.php?taxonomy=wpcm_season&amp;post_type=wpcm_player');
 		remove_submenu_page('edit.php?post_type=wpcm_player', 'edit-tags.php?taxonomy=wpcm_team&amp;post_type=wpcm_player');
@@ -39,11 +44,33 @@ class WPCM_Admin_Menus {
 	}
 
 	/**
+	 * Addons menu item
+	 */
+	public function extensions_menu() {
+		add_submenu_page( 'wpcm-settings', __( 'WP Club Manager Extensions', 'wpclubmanager' ),  __( 'Extensions', 'wpclubmanager' ) , 'manage_wpclubmanager', 'wpcm-extensions', array( $this, 'extensions_page' ) );
+	}
+
+	/**
+	 * Add menu item
+	 */
+	public function settings_menu() {
+		$settings_page = add_submenu_page( 'wpcm-settings', __( 'Settings', 'wpclubmanager' ),  __( 'Settings', 'wpclubmanager' ) , 'manage_wpclubmanager', 'wpcm-settings', array( $this, 'settings_page' ) );
+	}
+
+	/**
 	 * Init the settings page
 	 */
 	public function settings_page() {
 		include_once( 'class-wpcm-admin-settings.php' );
 		WPCM_Admin_Settings::output();
+	}
+
+	/**
+	 * Init the extensions page
+	 */
+	public function extensions_page() {
+		$page = include( 'class-wpcm-admin-extensions.php' );
+		$page->output();
 	}
 }
 
