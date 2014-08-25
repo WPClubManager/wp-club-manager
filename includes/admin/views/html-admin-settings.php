cf28d80c4c45c82e20acd52f2bede145cf9b5eb5
roofdog <leon@xenproject.com>
2014-08-25 18:11:35 +0100

    Remove settings sidebar

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -30,37 +30,7 @@
 
 				</div>
 
-				<div id="postbox-container-1" class="postbox-container">
-
-					<div id="wpcm-support" class="stuffbox ">
-						<h3>WP Club Manager Themes</h3>
-						<div class="inside">
-							<p>Improve your club site with our purpose built themes! Feature packed, responsive and highly customizable, <a href="https://wpclubmanager.com/themes/scoreline/" target="_blank">Scoreline</a> is our first theme ready-made for WP Club Manager.</p>
-							<p><a href="https://wpclubmanager.com/themes/scoreline/" target="_blank">Check it out!</a></p>
-						</div>
-					</div>
-
-					<div id="wpcm-support" class="stuffbox ">
-						<h3>Looking for support?</h3>
-						<div class="inside">
-							<p>Make sure to have a look at the plugin <a href="http://wpclubmanager.com/docs/" target="_blank">documentation</a> or ask any questions in our <a href="http://wpclubmanager.com/support/" target="_blank">support forums</a>.</p>
-						</div>
-					</div>
-					
-					<div id="wpcm-like-plugin" class="stuffbox ">
-						<h3>Like this plugin?</h3>
-						<div class="inside">
-							<p>Please consider showing your appreciation by helping to spread the word.</p>
-							<ul class="ul-disc">
-								<li><a target="_blank" href="http://wordpress.org/support/view/plugin-reviews/wp-club-manager?rate=5#postform">Leave a &#9733;&#9733;&#9733;&#9733;&#9733; plugin review on WordPress.org</a></li>
-								<li><a target="_blank" href="http://twitter.com/?status=Showing%20my%20appreciation%20for%20WordPress%20plugin%3A%20WP%20Club%20Manager%20%20-%20check%20it%20out!%20http%3A%2F%2Fwordpress.org%2Fplugins%2Fwp-club-manager%2F">Tweet about WP Club Manager</a></li>
-								<li><a target="_blank" href="http://wordpress.org/plugins/wp-club-manager/">Vote "works" on the WordPress.org plugin page</a></li>
-							</ul>
-							<p>You can also follow WP Club Manager on Twitter <a href="https://twitter.com/WPClubManager" target="_blank">here</a>.</p>
-						</div>
-					</div>
-
-				</div>
+				<?php include 'html-admin-sidebar.php'; ?>
 
 			</div>
 
