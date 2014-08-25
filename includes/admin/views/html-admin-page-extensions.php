924195c9aa33c9130698304ba619b2e7d93448c5
roofdog <leon@xenproject.com>
2014-08-25 18:04:26 +0100

    Add extensions page to admin

---
+++ includes/admin/views/html-admin-page-extensions.php
@@ -0,0 +1,31 @@
+<div class="wrap wpclubmanager-extensions">
+	<h2>
+		<?php _e( 'WP Club Manager Extensions', 'wpclubmanager' ); ?>
+		<a href="https://wpclubmanager.com/extensions/" class="add-new-h2"><?php _e( 'Browse all extensions', 'wpclubmanager' ); ?></a>
+		<a href="https://wpclubmanager.com/themes/" class="add-new-h2"><?php _e( 'Browse themes', 'wpclubmanager' ); ?></a>
+	</h2>
+
+	<p><?php _e( 'These add-ons extend the functionality of WP Club Manager.', 'wpclubmanager' ); ?></p>
+
+	<div class="">
+		<ul class="extensions">
+			<li class="extension">
+				<a href="https://wpclubmanager.com/extensions/score-summary">
+					<img src="<?php echo WPCM()->plugin_url(); ?>/assets/images/extensions/wpcm-ss-280.png"/>
+				</a>
+				<h3>Score Summary</h3>
+				<p>Keep record of the interval scores and add a score summary to your clubs matches</p>
+				<a href="https://wpclubmanager.com/extensions/score-summary" class="button right">Find out more</a>
+			</li>
+			<li class="extension">
+				<a href="https://wpclubmanager.com/extensions/player-appearances">
+					<img src="<?php echo WPCM()->plugin_url(); ?>/assets/images/extensions/wpcm-pa-280.png"/>
+				</a>
+				<h3>Player Appearances</h3>
+				<p>Display a list of a players matches, season by season, on their profile</p>
+				<a href="https://wpclubmanager.com/extensions/player-appearances" class="button right">Find out more</a>
+			</li>
+		</ul>
+	</div>
+
+</div>
