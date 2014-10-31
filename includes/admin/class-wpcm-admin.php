c44a5408f3b3edbf3581a4d1f4428411d88b5713
roofdog <leon@xenproject.com>
2014-10-31 12:36:23 +0000

    Prevent admin access

--- includes/admin/class-wpcm-admin.php
+++ includes/admin/class-wpcm-admin.php
@@ -21,6 +21,8 @@ class WPCM_Admin {
 	public function __construct() {
 
 		add_action( 'init', array( $this, 'includes' ) );
+		add_action( 'current_screen', array( $this, 'conditonal_includes' ) );
+		add_action( 'admin_init', array( $this, 'prevent_admin_access' ) );
 	}
 
 	/**
@@ -41,6 +43,43 @@ class WPCM_Admin {
 			include( 'class-wpcm-admin-notices.php' );
 			include( 'class-wpcm-admin-assets.php' );
 			include( 'class-wpcm-admin-editor.php' );
+		}
+	}
+
+	/**
+	 * Include admin files conditionally
+	 */
+	public function conditonal_includes() {
+		$screen = get_current_screen();
+
+		switch ( $screen->id ) {
+			case 'dashboard' :
+				//include( 'class-wpcm-admin-dashboard.php' );
+			break;
+			case 'users' :
+			case 'user' :
+			case 'profile' :
+			case 'user-edit' :
+				//include( 'class-wpcm-admin-profile.php' );
+			break;
+		}
+	}
+
+	/**
+	 * Prevent any user who cannot 'edit_posts' (subscribers, supporters etc) from accessing admin
+	 */
+	public function prevent_admin_access() {
+		$prevent_access = false;
+
+		if ( 'yes' == get_option( 'wpclubmanager_lock_down_admin' ) && ! is_ajax() && ! ( current_user_can( 'edit_posts' ) || current_user_can( 'manage_wpclubmanager' ) ) && basename( $_SERVER["SCRIPT_FILENAME"] ) !== 'admin-post.php' ) {
+			$prevent_access = true;
+		}
+
+		$prevent_access = apply_filters( 'wpclubmanager_prevent_admin_access', $prevent_access );
+
+		if ( $prevent_access ) {
+			wp_safe_redirect( get_permalink( wpcm_get_page_id( 'myaccount' ) ) );
+			exit;
 		}
 	}
 }
