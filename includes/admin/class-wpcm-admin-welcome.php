93043f573ff23127da77c1884d0d7c4aa34b9d52
roofdog <leon@xenproject.com>
2014-12-28 19:19:31 +0000

    Update translators

--- includes/admin/class-wpcm-admin-welcome.php
+++ includes/admin/class-wpcm-admin-welcome.php
@@ -542,7 +542,7 @@ class WPCM_Admin_Welcome {
 				<div class="wpcm-feature feature-section col three-col">
 
 					<?php
-					$translator_handles = array( 'ClubPress', 'King3R', 'rychu_cmg', 'fvottendorf', 'Spirossmil', 'lucabarbetti', 'baldovi', 'thegreat', 'Vadim_C', 'hushiea', 'Pirolla', 'hatasu', 'piotr01', 'cherreman', 'Wuko', 'sigma66699969669', 'Kauge', 'SzJanos', 'Abdulelah', 'mwestrik', 'GhiMax', 'lasiton' );
+					$translator_handles = array( 'ClubPress', 'King3R', 'rychu_cmg', 'fvottendorf', 'Spirossmil', 'lucabarbetti', 'baldovi', 'thegreat', 'Vadim_C', 'hushiea', 'Pirolla', 'hatasu', 'piotr01', 'cherreman', 'Wuko', 'sigma66699969669', 'Kauge', 'SzJanos', 'Abdulelah', 'mwestrik', 'GhiMax', 'lasiton', 'Akinshin', 'retbys', 'sivasspor', 'cengizyilmaz', 'cemsacan', 'muhahmetkara' );
 					$translator_links = array();
 					foreach ( $translator_handles as $handle ):
 						$translator_links[] = '<a href="https://www.transifex.com/accounts/profile/' . $handle . '">' . $handle . '</a>';
