c3665b173e94e80c92c1ba681137028522cd690e
roofdog <leon@wpclubmanager.com>
2019-12-30 12:44:19 +0000

    Update wpclubmanager.php
    Update version

--- wpclubmanager.php
+++ wpclubmanager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Club Manager
- * Version: 2.1.7-beta
+ * Version: 2.1.8
  * Plugin URI: https://wpclubmanager.com
  * Description: A plugin to help you run a sports club website easily and quickly.
  * Author: Clubpress
@@ -31,7 +31,7 @@ final class WPClubManager {
 	/**
 	 * @var string
 	 */
-	public $version = '2.1.7-beta';
+	public $version = '2.1.8';
 
 	/**
 	 * @var WPClubManager The single instance of the class
