356257777a3c3d1b860a2c35972b16c3a30804cb
polevaultweb <iain@polevaultweb.com>
2026-05-12 02:12:51 +0000

    Test: block registration for all WPCM shortcodes (#43)

---
+++ tests/wpunit/BlockRegistrationTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Tests for Gutenberg block registration.
+ *
+ * Verifies all WPCM shortcodes have corresponding Gutenberg blocks
+ * with render callbacks that produce output.
+ */
+
+class BlockRegistrationTest extends WPCMTestCase {
+
+	/** @dataProvider block_names */
+	public function test_block_is_registered( $block_name ) {
+		$registry = WP_Block_Type_Registry::get_instance();
+		$this->assertTrue(
+			$registry->is_registered( $block_name ),
+			"Block {$block_name} should be registered"
+		);
+	}
+
+	/** @dataProvider block_names */
+	public function test_block_has_callable_render_callback( $block_name ) {
+		$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
+		$this->assertNotNull( $block_type, "Block {$block_name} should exist" );
+		$this->assertTrue(
+			is_callable( $block_type->render_callback ),
+			"Block {$block_name} should have a callable render callback"
+		);
+	}
+
+	/** @dataProvider block_names */
+	public function test_block_render_returns_string( $block_name ) {
+		$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
+		$output     = call_user_func( $block_type->render_callback, array() );
+		$this->assertIsString( $output, "Block {$block_name} render should return a string" );
+	}
+
+	public function block_names() {
+		return array(
+			array( 'wpcm/match-list' ),
+			array( 'wpcm/player-list' ),
+			array( 'wpcm/player-gallery' ),
+			array( 'wpcm/staff-list' ),
+			array( 'wpcm/staff-gallery' ),
+			array( 'wpcm/league-table' ),
+			array( 'wpcm/map-venue' ),
+		);
+	}
+
+	public function test_match_opponents_block_registered_in_club_mode() {
+		if ( ! function_exists( 'is_club_mode' ) || ! is_club_mode() ) {
+			$this->markTestSkipped( 'match-opponents block requires club mode' );
+		}
+		$registry = WP_Block_Type_Registry::get_instance();
+		$this->assertTrue( $registry->is_registered( 'wpcm/match-opponents' ) );
+	}
+
+	public function test_block_category_is_registered() {
+		$post_id = wp_insert_post( array(
+			'post_type'   => 'post',
+			'post_title'  => 'Block Category Test',
+			'post_status' => 'draft',
+		) );
+
+		$categories = apply_filters( 'block_categories_all', array(), get_post( $post_id ) );
+		$slugs      = wp_list_pluck( $categories, 'slug' );
+		$this->assertContains( 'wp-club-manager', $slugs );
+
+		wp_delete_post( $post_id, true );
+	}
+
+	/** @dataProvider block_names */
+	public function test_block_has_editor_script( $block_name ) {
+		$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
+		$this->assertNotNull( $block_type );
+		$this->assertContains( 'wpcm-blocks-editor', $block_type->editor_script_handles );
+	}
+}
