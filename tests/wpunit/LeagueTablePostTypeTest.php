db62c1e61aac22bfa69fe19ae39fd52a6a0911ba
polevaultweb <iain@polevaultweb.com>
2026-05-14 00:06:01 +0000

    Test: league table post type public visibility (#82)

---
+++ tests/wpunit/LeagueTablePostTypeTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Tests for league table (wpcm_table) post type public visibility.
+ *
+ * @see https://github.com/WPClubManager/wp-club-manager/issues/82
+ */
+
+class LeagueTablePostTypeTest extends WPCMTestCase {
+
+	public function test_league_table_post_type_is_public() {
+		$obj = get_post_type_object( 'wpcm_table' );
+		$this->assertNotNull( $obj );
+		$this->assertTrue( $obj->public, 'wpcm_table should be public' );
+	}
+
+	public function test_league_table_post_type_is_publicly_queryable() {
+		$obj = get_post_type_object( 'wpcm_table' );
+		$this->assertNotNull( $obj );
+		$this->assertTrue( $obj->publicly_queryable, 'wpcm_table should be publicly queryable' );
+	}
+
+	public function test_league_table_post_type_has_query_var() {
+		$obj = get_post_type_object( 'wpcm_table' );
+		$this->assertNotNull( $obj );
+		$this->assertNotFalse( $obj->query_var, 'wpcm_table should have a query var' );
+	}
+
+	public function test_league_table_post_type_has_rewrite() {
+		$obj = get_post_type_object( 'wpcm_table' );
+		$this->assertNotNull( $obj );
+		$this->assertNotFalse( $obj->rewrite, 'wpcm_table should have rewrite rules' );
+	}
+
+	public function test_league_table_default_rewrite_slug() {
+		delete_option( 'wpclubmanager_table_slug' );
+		// Re-register post types to pick up default slug.
+		$post_types = new WPCM_Post_Types();
+		$post_types->register_post_types();
+
+		$obj = get_post_type_object( 'wpcm_table' );
+		$this->assertNotNull( $obj );
+		$this->assertIsArray( $obj->rewrite );
+		$this->assertEquals( 'table', $obj->rewrite['slug'] );
+	}
+
+	public function test_league_table_custom_rewrite_slug() {
+		update_option( 'wpclubmanager_table_slug', 'league-table' );
+		// Re-register post types to pick up custom slug.
+		$post_types = new WPCM_Post_Types();
+		$post_types->register_post_types();
+
+		$obj = get_post_type_object( 'wpcm_table' );
+		$this->assertNotNull( $obj );
+		$this->assertIsArray( $obj->rewrite );
+		$this->assertEquals( 'league-table', $obj->rewrite['slug'] );
+
+		delete_option( 'wpclubmanager_table_slug' );
+	}
+
+	public function test_league_table_single_page_resolves() {
+		$post_id = wp_insert_post( array(
+			'post_type'   => 'wpcm_table',
+			'post_title'  => 'Premier League',
+			'post_status' => 'publish',
+		) );
+
+		$this->assertGreaterThan( 0, $post_id );
+
+		$permalink = get_permalink( $post_id );
+		$this->assertNotEmpty( $permalink );
+		$this->assertStringContainsString( 'table', $permalink );
+
+		wp_delete_post( $post_id, true );
+	}
+}
