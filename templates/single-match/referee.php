646ad506e0dbcb7c09e8e4d98c78b15b9aff4d94
DAnn2012 <dannunziata@hotmail.it>
2026-05-30 11:22:10 +0200

    Removed double echo from 3 files (#131)
    Fix: remove redundant echo before esc_html_e() in match templates

--- templates/single-match/referee.php
+++ templates/single-match/referee.php
@@ -20,7 +20,7 @@ if ( $referee && 'yes' === $show_referee ) { ?>
 
 	<div class="wpcm-match-referee">
 
-		<?php echo esc_html_e( 'Referee', 'wp-club-manager' ); ?>: <?php echo esc_html( $referee ); ?>
+		<?php esc_html_e( 'Referee', 'wp-club-manager' ); ?>: <?php echo esc_html( $referee ); ?>
 
 	</div>
 
